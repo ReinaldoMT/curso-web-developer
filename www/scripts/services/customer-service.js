d4bc99f277c6e816d2f363ec0c79a8a7dd453c2f
Reinaldo Martins <reinaldo.martins@gmail.com>
2019-09-19 11:50:44 -0300

    Services e gravação de dados

---
+++ www/scripts/services/customer-service.js
@@ -0,0 +1,37 @@
+class CustomerService {
+  constructor () {
+    const jsonData = localStorage.getItem('customers')
+    this.data = JSON.parse(jsonData) || []
+  }
+
+  /**
+   * Retorna um identificador único
+   * @example
+   * getUid() // Retorna 1234567899
+   */
+  getUid () {
+    return new Date().getTime().toString()
+  }
+
+  getById (id) {
+    return this.data.find(x => x.id === id)
+  }
+
+  get () {
+    return this.data
+  }
+
+  set (data) {
+    if (data.id) {
+      const index = this.data.findIndex(x => x.id === data.id)
+      this.data[index] = data
+    } else {
+      // Se eu não tenho id no data, então é um novo registro
+      // Neste caso eu crio um novo uid para os novos dados
+      data.id = this.getUid()
+      this.data.push(data)
+    }
+    const jsonData = JSON.stringify(this.data)
+    localStorage.setItem('customers', jsonData)
+  }
+}
